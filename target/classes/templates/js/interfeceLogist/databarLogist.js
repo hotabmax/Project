e57c95dfc8b01d67eb6f13fd172df2add72be5a0
hotabmax <maksim.solovev.99@inbox.ru>
2022-03-30 23:03:57 +0400

    changes v 7.1

--- target/classes/templates/js/interfeceLogist/databarLogist.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function (){
-    $('body').append("<div id='databar'; style='width: 90%; min-height: 300px; font-family: Arial; float: left ; background-color: #f8f8f8; border-radius: 30px; margin-top: 50px; margin-left: 5%; padding-bottom: 50px'>" +
-        "<div style='text-align: center;'><h1>Товары</h1></div>" +
-        "</div>")
-})
