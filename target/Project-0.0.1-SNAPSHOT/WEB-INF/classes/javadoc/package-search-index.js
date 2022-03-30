ed4bb34469ed544347b558af50d1832201ab79bb
hotabmax <maksim.solovev.99@inbox.ru>
2022-03-30 23:15:45 +0400

    changes v 7.3

--- target/Project-0.0.1-SNAPSHOT/WEB-INF/classes/javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com"},{"l":"com.filters"},{"l":"com.jUnitTests"},{"l":"com.models"},{"l":"com.repositories"},{"l":"com.rest"},{"l":"com.services"}];updateSearchResults();
--- target/Project-0.0.1-SNAPSHOT/WEB-INF/classes/templates/js/interfeceLogist/menubarLogist.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function (){
-    $('body').append("<div id='menubar' style='min-width: 19%; height: 100px; font-family: Arial; float: right; border-radius: 10px; margin-top: 50px; margin-right: 5%'></div>")
-})
