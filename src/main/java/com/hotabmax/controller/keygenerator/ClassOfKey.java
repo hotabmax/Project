9d4335d423424c12d21596c8e39d6639798a368e
hotabmax <maksim.solovev.99@inbox.ru>
2022-04-01 12:28:11 +0400

    changes v 7.5

--- src/main/java/com/hotabmax/controller/keygenerator/ClassOfKey.java
+++ src/main/java/com/hotabmax/controller/keygenerator/ClassOfKey.java
@@ -2,20 +2,27 @@ package com.hotabmax.controller.keygenerator;
 
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
 
 @Service
-@Component("ClassOfKey")
-public class ClassOfKey {
+@Component("ClassOfKeys")
+public class ClassOfKeys {
     private Key key;
+    private KeyPair keyPair;
 
-    public Key getKey() {
+    public Key getKey() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
         if(this.key == null){
             this.key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+            KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+            generator.initialize(2048);
+            this.keyPair = generator.generateKeyPair();
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
         }
         return this.key;
     }
